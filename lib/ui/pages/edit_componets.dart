041f46a12a0085aca2f8cb21efda5c2124311c78
Combatwombat77 <56967530+Combatwombat77@users.noreply.github.com>
2020-01-08 20:34:06 +0700

    essentially the same thing plus the swot grid and naviagtion buttons
    Co-Authored-By: Isheanesu Gwangwanyu <ishe19@users.noreply.github.com>

---
+++ lib/ui/pages/edit_componets.dart
@@ -0,0 +1,74 @@
+import 'package:copy_cat/ui/swot_home.dart';
+import 'package:copy_cat/ui/utils/uidata.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../dashboard.dart';
+
+class SwotGrid extends StatelessWidget{
+  
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: (){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Dashboard())
+            );
+          },
+        ),
+        title: Text('SWOT ANALYSIS'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.done),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard()));
+              },
+          )
+        ],
+      ),
+      body: OrientationBuilder(builder: (context, orientaion){
+        return GridView.count(     
+        padding: const EdgeInsets.all(20.0),
+        crossAxisSpacing: 10.0,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Center(
+             child: Card(
+               elevation: 5.0,
+               color: Colors.green,
+               child: Text('Strengths'),
+               ),
+               ),
+               Center(
+             child: Card(
+               elevation: 5.0,
+               color: Colors.blue,
+               child: Text('Weaknesses'),
+               ),
+               ),
+               Center(
+             child: Card(
+               elevation: 5.0,
+               color: Colors.orange,
+               child: Text('Opportunities'),
+               ),
+               ),
+               Center(
+             child: Card(
+               elevation: 5.0,
+               color: Colors.purple,
+               child: Text('Threats'),
+               ),
+               ),
+        ],
+        );
+      }
+      )
+      )
+      );
+      }
+  }
+   
+
